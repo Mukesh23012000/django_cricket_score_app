0bd1e7a176086258caf7b2e3aedafae00bbb5879
Mukesh23012000 <sendtomk1@gmail.com>
2022-11-06 10:26:45 +0530

    updates

--- prob/urls.py
+++ prob/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 from fileupload import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name ='index'),
@@ -9,4 +8,3 @@ urlpatterns = [
     path('back',views.index,name ='index'),
     path('check',views.check),
 ]
-urlpatterns += staticfiles_urlpatterns()
